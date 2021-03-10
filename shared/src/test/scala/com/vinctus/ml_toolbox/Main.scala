b423eb7056a2eede370774957225771d17008cbd
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-03-10 13:20:42 -0500

    MLRD-4 cont coding linear regression

--- shared/src/test/scala/com/vinctus/ml_toolbox/Main.scala
+++ shared/src/test/scala/com/vinctus/ml_toolbox/Main.scala
@@ -2,7 +2,7 @@ package com.vinctus.ml_toolbox
 
 object Main extends App {
 
-  val ds = Dataset(Seq("t", "a"), Seq(Seq(2, 1), Seq(4, 2)))
+  val ds = Dataset(Seq("t", "a"), Seq(Seq(4, 2), Seq(6, 3)))
 //  val ds = Dataset.fromCSV("test")
 
   val model = LinearRegression.train(ds)
