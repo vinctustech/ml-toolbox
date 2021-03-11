1e4cc0f8b3d741c88ae15d838ecf53593a44fb24
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-03-11 14:35:22 -0500

    MLRD-4 start coding simple plotting support

---
+++ shared/src/main/scala/com/vinctus/ml_toolbox/Plot.scala
@@ -0,0 +1,10 @@
+package com.vinctus.ml_toolbox
+
+class Plot(val xlower: Double,
+           val xupper: Double,
+           val ylower: Double,
+           val yupper: Double,
+           val scale: Double = 1,
+           val xlabeling: Double = 1,
+           val ylabeling: Double = 1,
+           val fontSize: Int = 10) {}
