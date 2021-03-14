0b50be564a55083623bb2b2c4f00e9e541d466b3
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-03-14 12:49:13 -0400

    MLRD-4 platform specific graphical code

---
+++ jvm/src/main/scala/com/vinctus/ml_toolbox/PlotPanel.scala
@@ -0,0 +1,75 @@
+package com.vinctus.ml_toolbox
+
+import java.awt.{BasicStroke, Color, Font, RenderingHints}
+import java.awt.font.FontRenderContext
+import java.awt.geom.{Line2D, Path2D}
+import scala.swing.{Graphics2D, Panel}
+import scala.swing.Swing.EtchedBorder
+import scala.swing.Swing._
+
+class PlotPanel(plot: Plot) extends Panel {
+
+  private val FRC = new FontRenderContext(null, true, false)
+  private val STYLE_MAP =
+    Map[Plot.Style, Int](
+      Plot.PLAIN -> Font.PLAIN,
+      Plot.ITALIC -> Font.ITALIC
+    )
+
+  background = Color.BLACK
+  border = EtchedBorder
+  preferredSize = (plot.width, plot.height)
+  plot.onChange = () => repaint()
+
+  override protected def paintComponent(g: Graphics2D): Unit = {
+    super.paintComponent(g)
+
+    g.setRenderingHints(
+      new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON))
+    g.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON))
+    g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE)
+
+    // draw points
+    g.setStroke(new BasicStroke(plot.pointSize.toFloat, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
+
+    plot.pointsIterator foreach {
+      case plot.Point(x, y, c) =>
+        g.setColor(new Color(c))
+        g.draw(new Line2D.Double(x, y, x, y))
+    }
+
+    // draw paths
+    plot.pathsIterator foreach { p =>
+      val path = new Path2D.Double
+      val (x, y) = p.start
+
+      path.moveTo(x, y)
+
+      p.rest foreach {
+        case (x, y) => path.lineTo(x, y)
+      }
+
+      g.setColor(new Color(p.color))
+      g.setStroke(new BasicStroke(p.width.toFloat, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
+      g.draw(path)
+    }
+
+    // draw text
+    plot.textsIterator foreach {
+      case plot.Text(s, x, y, color, style, pos) =>
+        val gv = new Font(Font.SERIF, STYLE_MAP(style), plot.fontSize).createGlyphVector(FRC, s)
+        val vb = gv.getVisualBounds
+        val (xp, yp) =
+          pos match {
+            case Plot.ABOVE => (x - vb.getCenterX, y)
+            case Plot.BELOW => (x - vb.getCenterX, y + vb.getHeight)
+            case Plot.RIGHT => (x - vb.getX, y - vb.getCenterY)
+            case Plot.LEFT  => (x - vb.getWidth - vb.getX, y - vb.getCenterY)
+          }
+
+        g.setColor(new Color(color))
+        g.drawGlyphVector(gv, xp.toFloat, yp.toFloat)
+    }
+  }
+
+}
--- jvm/src/test/scala/com/vinctus/ml_toolbox/Main.scala
+++ jvm/src/test/scala/com/vinctus/ml_toolbox/Main.scala
@@ -1,13 +1,6 @@
 package com.vinctus.ml_toolbox
 
-import xyz.hyperreal.matrix.Matrix
-
-import java.awt.font.FontRenderContext
-import scala.swing.{Frame, Graphics2D, MainFrame, Panel, SimpleSwingApplication}
-import scala.swing.Swing._
-import java.awt.geom.{Line2D, Path2D}
-import java.awt.{BasicStroke, Color, Font, RenderingHints}
-import scala.math._
+import scala.swing.{Frame, MainFrame, SimpleSwingApplication}
 
 //object Main extends App {
 //
@@ -66,71 +59,4 @@ object Main extends SimpleSwingApplication {
       contents = new PlotPanel(plot)
       pack()
     }
-}
-
-class PlotPanel(plot: Plot) extends Panel {
-
-  private val FRC = new FontRenderContext(null, true, false)
-  private val STYLE_MAP =
-    Map[Plot.Style, Int](
-      Plot.PLAIN -> Font.PLAIN,
-      Plot.ITALIC -> Font.ITALIC
-    )
-
-  background = Color.BLACK
-  border = EtchedBorder
-  preferredSize = (plot.width, plot.height)
-  plot.onChange = () => repaint()
-
-  override protected def paintComponent(g: Graphics2D): Unit = {
-    super.paintComponent(g)
-
-    g.setRenderingHints(
-      new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON))
-    g.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON))
-    g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE)
-
-    // draw points
-    g.setStroke(new BasicStroke(plot.pointSize, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
-
-    plot.pointsIterator foreach {
-      case plot.Point(x, y, c) =>
-        g.setColor(new Color(c))
-        g.draw(new Line2D.Double(x, y, x, y))
-    }
-
-    // draw paths
-    plot.pathsIterator foreach { p =>
-      val path = new Path2D.Double
-      val (x, y) = p.start
-
-      path.moveTo(x, y)
-
-      p.rest foreach {
-        case (x, y) => path.lineTo(x, y)
-      }
-
-      g.setColor(new Color(p.color))
-      g.setStroke(new BasicStroke(p.width.toFloat, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
-      g.draw(path)
-    }
-
-    // draw text
-    plot.textsIterator foreach {
-      case plot.Text(s, x, y, color, style, pos) =>
-        val gv = new Font(Font.SERIF, STYLE_MAP(style), plot.fontSize).createGlyphVector(FRC, s)
-        val vb = gv.getVisualBounds
-        val (xp, yp) =
-          pos match {
-            case Plot.ABOVE => (x - vb.getCenterX, y)
-            case Plot.BELOW => (x - vb.getCenterX, y + vb.getHeight)
-            case Plot.RIGHT => (x - vb.getX, y - vb.getCenterY)
-            case Plot.LEFT  => (x - vb.getWidth - vb.getX, y - vb.getCenterY)
-          }
-
-        g.setColor(new Color(color))
-        g.drawGlyphVector(gv, xp.toFloat, yp.toFloat)
-    }
-  }
-
 }
